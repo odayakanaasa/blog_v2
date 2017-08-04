5fc3f4e61ebe35d1dd7477e0050b17a7779eb7e1
hlz <tiemaocsdn@qq.com>
2017-08-04 23:44:06 +0800

    Handle sql auto bak

--- __materials/bat/blog_shell.sh
+++ __materials/bat/blog_shell.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-## 依据我的线上环境写的crontab计划
-##### minute - 从0到59的整数 
-##### hour - 从0到23的整数 
-##### day - 从1到31的整数 (必须是指定月份的有效日期)
-##### month - 从1到12的整数 (或如Jan或Feb简写的月份)
-##### dayofweek - 从0到7的整数，0或7用来描述周日 (或用Sun或Mon简写来表示)
-##### command - 需要执行的命令(可用as ls /proc >> /tmp/proc或 执行自定义脚本的命令) 
-
-## crontab set time 03:00:00 to update in daily
-## 0 3 * * * /root/shells/blog_shell.sh
-
-## variable 
 last_day=`date -d last-day +%Y%m%d`
-
 # Create sitemap
 php /data/www/blog/public/index.php Cli/sitemap
 
@@ -24,6 +11,8 @@ mv ${log_path}/access.log ${log_path}/daily_acc_logs/${last_day}.log
 touch ${log_path}access.log
 ${nginx}/sbin/nginx -c ${nginx}/conf/nginx.conf -s reload
 
-
+mysql_dump_path="/data/www/blog/__materials/sql_baks"
 # backup database  in raw
-zip -r /data/www/blog/__materials/sql_baks/${last_day}.zip /usr/local/mysql/data/yth_blog/*
+mysqldump --sock=/tmp/mysql.sock -u用户 -p密码 yth_blog > yth_blog.sql 
+tar -zcvf ${mysql_dump_path}/${last_day}.tar.gz yth_blog.sql 
+rm -rf ${mysql_dump_path}/yth_blog.sql 
