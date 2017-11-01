93322d1cc2b4d072c9609fabe7c15826c7b004d0
HaleyLeoZhang <229270575@qq.com>
2017-11-01 10:00:29 +0800

    Add email tpl

--- application/controller/Cli.php
+++ application/controller/Cli.php
@@ -28,41 +28,46 @@ class Cli{
 	}
 
 	// 构造函数
-	public function __construct(){
-		// Clis模式才能运行
-		if(  !preg_match('/cli/i', php_sapi_name() )  ){
-			header("Location: /404.html");
-			exit();
-		}
-	}
+	// public function __construct(){
+	// 	// Clis模式才能运行
+	// 	if(  !preg_match('/cli/i', php_sapi_name() )  ){
+	// 		header("Location: /404.html");
+	// 		exit();
+	// 	}
+	// }
 	// 析构函数
 	public function __destruct(){
-    print(PHP_EOL."Time: ".date("Y/m/d H:m:s").PHP_EOL );
+    echo PHP_EOL.'Time: '.date("Y/m/d H:m:s").PHP_EOL ;
 	}
 
   // [每天凌晨0点] 生成sitemap.xml
   public function sitemap(){
     $Sitemap = new \app\logic\cli_sitemap();
     $Sitemap->run();
-    print("Sitemap created." );
+    echo 'Sitemap created.' ;
   }
 
 
   // [每天凌晨0点] 发送压缩好的备份数据库到邮箱，并删除30天前的
+    // 命令行运行php，与服务器 生成的日志文件，权限不同，暂停书写日志
   public function bak_sql_to_email(){
     $last_day = date("Ymd", strtotime("-1 day") );
+    $set_file_name = $last_day. '.tar.gz';
     $to = 'myboyli4@163.com';
     $title = 'blog v2.0 - 数据备份';
-    $content = '<h2>资源日期:</h2>'. $last_day;
-    $set_file_name = $last_day. '.tar.gz';
+    // 写入html模板
+    $v  = new View;
+    $v->file_name = $set_file_name;
+    $content = $v->fetch('Email_tpl/bak_sql');
+
     $file_path =  ROOT_PATH. '__materials/sql_baks/'. $set_file_name  ;
     $path = realpath( $file_path );
     if( is_file($path) ){
       $status = Smtp::send($to, $title, $content, $path, $set_file_name);
       if( $status ){
-        Log::out('Send sql_bak file to the email success', 1);
+        // Log::out('Send sql_bak file to the email success', 1);
       }else{
-        Log::out('Send sql_bak file to the email failed', 1);
+        // Log::out('Send sql_bak file to the email failed', 1);
       }
       $before_30_day = date("Ymd", strtotime("-30 day") );
       $set_file_name = $before_30_day. '.tar.gz';
@@ -70,7 +75,7 @@ class Cli{
       $path = realpath( $file_path );
       @unlink($path);
     }else{
-      Log::out('No such a sql_bak file', 1);
+      // Log::out('No such a sql_bak file', 1);
     }
 
   }
