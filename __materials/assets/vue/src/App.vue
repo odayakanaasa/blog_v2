ea89c537a8585d048405c499a740584466662ad5
HaleyLeoZhang <229270575@qq.com>
2018-02-14 00:03:00 +0800

    Updated

--- __materials/assets/vue/src/App.vue
+++ __materials/assets/vue/src/App.vue
@@ -1,58 +1,54 @@
 <template>
-    <div id="app">
-        <!-- 这里是全局视图 -->
-        <!-- 给予视图过渡效果 -->
-        <transition name="slide-left">
-            <!-- 这里是 router里面的视图 -->
-            <router-view></router-view>
-        </transition>
-    </div>
+  <div id="app">
+    <!-- 这里是全局视图 -->
+    <!-- 给予视图过渡效果 -->
+    <transition name="slide-fade">
+      <!-- 这里是 router里面的视图 -->
+      <router-view></router-view>
+    </transition>
+  </div>
 </template>
 <script>
 export default {
-    name: 'App'
+  name: 'App'
 }
+
 </script>
-<style>
-html,body {
-  height: 100%;
-}
-
-a.active {
-  text-decoration: none;
-}
-
+<style lang="scss" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   height: 100%;
   background: #F5F5F5;
-  .content{
-    padding-top: px2rem(100px);
-    background: #F5F5F5;
+  .slide-fade-enter-active {
+    animation: fadeInDown .4s
   }
-  .tabar {
-    margin-bottom: px2rem(120px);
+  .slide-fade-leave-active {
+    animation: fadeOutDown .1s
   }
-  .slide-left-enter-active,
-  .slide-left-leave-active {
-    transition: all .1s ease-in;
-    opacity: 1;
-  }
-  .slide-left-enter,
-  .slide-left-leave-active {
+}
+
+
+
+@keyframes fadeInDown {
+  0% {
     opacity: 0;
+    transform: translateY(-20px)
   }
-
-}
-
-@keyframes slideLeft {
-  from {
-    transform: translate3d(100%, 0, 0);
-    visibility: visible;
-  }
-
-  to {
-    transform: translate3d(0, 0, 0);
+  100% {
+    opacity: 1;
+    transform: translateY(0)
   }
 }
+
+@keyframes fadeOutDown {
+  0% {
+    opacity: 1;
+    transform: translateY(0)
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(20px)
+  }
+}
+
 </style>
